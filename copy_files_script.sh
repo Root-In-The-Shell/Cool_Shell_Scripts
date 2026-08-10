db82f1be93fbbea9d5b03e7b65064e96aa65892d
Glyn <glynlayton@gmail.com>
2026-08-10 18:24:04 +0100

    A: A new shell script

---
+++ copy_files_script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#to move groups of files starting with same letters
+
+SOURCE="/home/ishikawa/Documents/Test_1/"
+DEST="/home/ishikawa/Documents/Test_2/"
+
+echo "Copying files..."
+echo "..."
+echo "ooh i got it"
+cp "$SOURCE"R2* "$SOURCE"R0* "$DEST"/
+echo "File copy complete!"
